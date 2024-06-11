51de21888a8c0eefd0285f81d4c5e5e88e630ad8
gabriel Camargo <gabriel.camargoadv@gmail.com>
2024-06-11 00:57:28 -0300

    feat

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://backend-home-wzmq.onrender.com',
 });
 
 export const setAuthToken = (token: string) => {
